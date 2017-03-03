070c90617a07dd30251814be7b69e50fa82ea33a
Jose Cano Espinoza <jcanoespinoza@gmail.com>
2017-03-03 16:42:38 -0600

    Cambios en la parte grafica del proyecto

--- Sisfacturacion/Sisfacturacion.Grafica/Program.cs
+++ Sisfacturacion/Sisfacturacion.Grafica/Program.cs
@@ -16,7 +16,7 @@ namespace Sisfacturacion.Grafica
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new InicioSesion());
         }
     }
 }
